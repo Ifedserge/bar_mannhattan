fb3025a820ef67e9422d93fbe89bd156d7059bbf
Serge Fedorov <Lir2ooo5@yandex.ru>
2023-06-26 23:20:39 +0300

    add: news, adaptiv home page

--- script/pages/news.js
+++ script/pages/news.js
@@ -1,6 +1,7 @@
 export default function news () {
     const h1 = document.createElement('h1');
-    h1.innerHTML = 'news';
+    h1.classList.add('news__h1')
+    h1.innerHTML = 'Новостей <span>нет</span>';
 
     return h1
 }

3b666c23757bc41eecf0b1775dfcc87aa38c6442
Serge Fedorov <Lir2ooo5@yandex.ru>
2023-06-27 10:09:12 +0300

    add: menu page

--- script/pages/menu.js
+++ script/pages/menu.js
@@ -1,6 +1,20 @@
-export default function menu () {
-    const h1 = document.createElement('h1');
-    h1.innerHTML = 'menu';
-
-    return h1
+class Menu {
+    constructor(){
+        this.menu = null;
+        this.init = this.init.bind(this);
+    }
+    create(){
+        this.menu = document.createElement('div');
+        this.menu.classList.add('menu__page');
+        this.menu.innerHTML = `
+            <img src='./img/menu.jpg' alt='menu'>
+        `;
+    }
+    init(){
+        this.create();
+        return this.menu;
+    }
 }
+const menu = new Menu();
+const init = menu.init
+export default init;
